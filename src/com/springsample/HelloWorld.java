f479996740e6ae85ee152d5cfba3306c7dd52a6e
hiren <hpatel@mobiquityinc.com>
2018-07-17 16:26:27 +0530

    Read multiple beans in the same file

--- src/com/springsample/HelloWorld.java
+++ src/com/springsample/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.springsample;
 
+import com.springsample.models.Student;
 import com.springsample.models.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,6 +9,13 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         readUserData();
+        readStudentData();
+    }
+
+    private static void readStudentData() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student student = (Student) applicationContext.getBean("studentBean");
+        student.displayStudentNickName();
     }
 
     private static void readUserData() {
